b32cd09a1151198670e343e384e46a09a4de1352
Roman Bulinski <aboandrus@gmail.com>
2019-07-17 17:22:23 +0200

    Refactor code against codewars checking time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,32 @@ import java.util.List;
 
 public class Main {
 
-
-    String[] names = new String[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
-
-
     public static String WhoIsNext(String[] names, int n) {
+
         int peopleUmonut = names.length;
+        if (n < peopleUmonut) {
+            return names[n - 1];
+        } else {
+            int sequnce = 1;
+            int sumPeople = peopleUmonut;
+            int lastPortion = peopleUmonut;
 
-       if(n<peopleUmonut){
-           return  names[n-1];
-       }else{
-           int counter =1;
-           int sum = peopleUmonut;
-           while ( (sum + (sum*2)) < n) {
-               sum = sum + (sum*2);
-               counter++;
-           }
-           int rest = n-sum;
+            while ((sumPeople + (lastPortion * 2)) < n) {
+                lastPortion *= 2;
+                sumPeople += lastPortion;
+                sequnce *= 2;
+            }
+            int rest = n - sumPeople;
 
-           List<String> tempString = new ArrayList<>();
+            if (rest < (sequnce * 2)) {
+                return names[0];
+            } else if (rest % (sequnce * 2) == 0) {
+                return names[(int) (rest / (sequnce * 2)) - 1];
+            } else {
+                return names[(int) (rest / (sequnce * 2))];
+            }
 
-           for(int j=0; j<names.length; j++){
-               for(int i=0; i<counter+2; i++){
-                   tempString.add(names[j]);
-               }
-           }
-           return tempString.get(rest);
-       }
-       
-
+        }
     }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -12,11 +12,71 @@ public class MainTest {
         int n = 1;
         assertEquals("Sheldon", Main.WhoIsNext(names, n));
     }
+
     @Test
     public void test2() {
         String[] names = new String[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
         int n = 6;
         assertEquals("Sheldon", Main.WhoIsNext(names, n));
     }
+
+    @Test
+    public void test3() {
+        String[] names = new String[] { "a", "b" };
+        int n =4;
+        assertEquals("a", Main.WhoIsNext(names, n));
+    }
+
+    @Test
+    public void test3d() {
+        String[] names = new String[] { "a", "b" };
+        int e =15;
+        assertEquals("a", Main.WhoIsNext(names, e));
+    }
+
+    @Test
+    public void test3c() {
+        String[] names = new String[] { "a", "b" };
+        int r =14;
+        assertEquals("b", Main.WhoIsNext(names, r));
+    }
+
+
+    @Test
+    public void test3b() {
+        String[] names = new String[] { "a", "b" };
+        int y =10;
+        assertEquals("a", Main.WhoIsNext(names, y));
+    }
+
+
+    @Test
+    public void test3a() {
+        String[] names = new String[] { "a", "b" };
+        int m =5;
+        assertEquals("b", Main.WhoIsNext(names, m));
+    }
+
+    @Test
+    public void test4() {
+        String[] names = new String[] { "a", "b" };
+        int n =31;
+        assertEquals("a", Main.WhoIsNext(names, n));
+    }
+
+    @Test
+    public void test5() {
+        String[] names = new String[] { "a", "b","c" };
+        int n =21;
+        assertEquals("c", Main.WhoIsNext(names, n));
+    }
+
+    @Test
+    public void test6() {
+        String[] names = new String[] { "a", "b","c" };
+        int n =22;
+        assertEquals("a", Main.WhoIsNext(names, n));
+    }
+
 
 }
